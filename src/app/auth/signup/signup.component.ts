77a11a68c39c665d7c8a44caa75a2f86b23d1313
Victor Izquierdo <vhmizq@gmail.com>
2023-08-11 21:20:39 -0600

    add date picker

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
-})export class SignupComponent {
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  maxDate!: Date
+  
+  ngOnInit(): void {
+    this.limitMaxDate();
+  }
+
+  constructor() {}
 
   onSubmit(form: NgForm) {
     console.log(form.value);
+  }
+
+  limitMaxDate() {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,23 +1,27 @@
-import { NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   imports: [
     MatIconModule,
     MatButtonModule,
     MatInputModule,
-    MatFormFieldModule
-
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatFormFieldModule
-  ]
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
 })
 export class MaterialModule {}
