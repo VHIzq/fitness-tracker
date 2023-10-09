8360addae1f6d25f474b45ea52af9a7c287e7c98
Víctor Izquierdo <85770256+VHIzq@users.noreply.github.com>
2023-10-09 16:52:06 +0000

    selectors

--- src/app/trainnig/training.actions.ts
+++ src/app/trainnig/training.actions.ts
@@ -3,16 +3,30 @@ import { Exercise } from './exercise.model';
 
 export const SET_AVAILABLE_TRANINGS = '[Traninig] Sete Available Trainig';
 export const SET_FINISHED_TRAININGS = '[Training] Set Finished Training';
-export const START_TRAINIMG = '[Training] Start Training';
+export const START_TRAINING = '[Training] Start Training';
 export const STOP_TRAINING = '[Training] Stop Training';
 
+export class SetAvailableTraining implements Action {
+  readonly type = SET_AVAILABLE_TRANINGS;
+  constructor(public payload: Array<Exercise>) {}
+}
+
+export class SetFinishedTrainings implements Action {
+  readonly type = SET_FINISHED_TRAININGS;
+  constructor(public payload: Array<Exercise>) {}
+}
+
 export class StartTraining implements Action {
-  readonly type = START_TRAINIMG;
-  constructor (public payload: Array<Exercise>) {}
+  readonly type = START_TRAINING;
+  constructor(public payload: Exercise) {}
 }
 
 export class StopTrainings implements Action {
   readonly type = STOP_TRAINING;
 }
 
-export type TrainingActions = StartTraining | StopTrainings;
+export type TrainingActions =
+  | SetAvailableTraining
+  | SetFinishedTrainings
+  | StartTraining
+  | StopTrainings;
--- src/app/trainnig/training.reducer.ts
+++ src/app/trainnig/training.reducer.ts
@@ -1,26 +1,53 @@
 import { Action } from '@ngrx/store';
 import { Exercise } from './exercise.model';
 import * as fromRoot from '../app.reducer';
+import {
+  SET_AVAILABLE_TRANINGS,
+  SET_FINISHED_TRAININGS,
+  START_TRAINING,
+  STOP_TRAINING,
+  TrainingActions,
+} from './training.actions';
 
 export interface TrainingState {
   availableExercises: Array<Exercise>;
   finishedExercises: Array<Exercise>;
+  activeTrainig?: Exercise;
 }
 
 export interface State extends fromRoot.State {
   training: TrainingState;
 }
 
-export function authReducer(state = initialState, action: Action) {
+const initialState: TrainingState = {
+  availableExercises: [],
+  finishedExercises: [],
+  activeTrainig: undefined,
+};
+
+export function authReducer(state = initialState, action: TrainingActions) {
   switch (action.type) {
-    case SET_AUTHENTICATED:
+    case SET_AVAILABLE_TRANINGS:
       return {
-        isAuthenticated: true
+        ...state,
+        availableExercises: action.payload,
       };
 
-    case SET_UNAUTHENTICATED:
+    case SET_FINISHED_TRAININGS:
       return {
-        isAuthenticated: false
+        ...state,
+        finishedExercises: action.payload,
+      };
+
+    case START_TRAINING:
+      return {
+        ...state,
+        activeTrainig: action.payload,
+      };
+
+    case STOP_TRAINING:
+      return {
+        activeTrainig: null,
       };
     default: {
       return state;
@@ -28,4 +55,6 @@ export function authReducer(state = initialState, action: Action) {
   }
 }
 
-export const getIsAuth = (state: State) => state.isAuthenticated;
+export const getAvailableTrainings = (state: TrainingState) => state.availableExercises;
+export const getFinishedExercises = (state: TrainingState) => state.finishedExercises;
+export const getActiveTraining = (state: TrainingState) => state.activeTrainig;
